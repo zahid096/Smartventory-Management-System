607220f3943068431efb9cd12e8e05c9a97f61cd
zahid096 <mdzahidhasan096@gmail.com>
2025-11-06 23:09:38 +0600

    Small update

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'WJ28@krhps'
+app.config['MYSQL_PASSWORD'] = '********'
 app.config['MYSQL_DB'] = 'smartstore_new'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
